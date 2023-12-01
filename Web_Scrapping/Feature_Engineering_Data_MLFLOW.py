ad565e7c43114dc058c30c759605a05f7959526b
Büşra Sulukan <129671358+busrasulukan@users.noreply.github.com>
2023-12-01 23:45:21 +0300

    Mlflow for Model Creation Stages
    Success values of the operations performed in the Model Creation stages

---
+++ Web_Scrapping/Feature_Engineering_Data_MLFLOW.py
@@ -0,0 +1,33 @@
+import mlflow
+import mlflow.sklearn
+import mlflow.pyfunc
+from mlflow.tracking import MlflowClient
+from mlflow.entities import ViewType
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, roc_auc_score, recall_score, precision_score, f1_score
+
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+mlflow_server_url = "http://127.0.0.1:5000"
+
+# Read data into DataFrame
+df = pd.read_csv("Feature_Engineering_Data.csv")
+
+# Assuming smaller_df1 is your DataFrame
+lsm = smf.ols('log_Fiyat ~ Yaş + Kilometre + Q("Motor Hacmi") + Q("Motor Gücü") + Q("Ortalama Yakıt Tüketimi") + Q("Depo Hacmi")  + Ford + Opel + Renault + Skoda + Toyota + Volkswagen + Q("Vites Tipi") + Q("Hasar Durumu") + Depo_Hacmi_Kullanabilirliği + Ters_Motor_Gücü + Yakıt_Ekonomisi + Engine_Ratio', data=df)
+fit1 = lsm.fit()
+
+
+# Get the R-squared score
+r2_score = fit1.rsquared
+
+print("R-squared Score:", r2_score)
+
+# Log R-squared score using MLflow
+with mlflow.start_run():
+    mlflow.log_param("Model", "Linear Regression")
+    mlflow.log_metric("R2 Score", r2_score)
---
+++ Web_Scrapping/Raw_Data_MLFLOW.py
@@ -0,0 +1,32 @@
+import mlflow
+import mlflow.sklearn
+import mlflow.pyfunc
+from mlflow.tracking import MlflowClient
+from mlflow.entities import ViewType
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, roc_auc_score, recall_score, precision_score, f1_score
+
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+mlflow_server_url = "http://127.0.0.1:5000"
+
+# Read data into DataFrame
+df = pd.read_csv("Raw_Data.csv")
+
+# Assuming smaller_df1 is your DataFrame
+lsm = smf.ols('Fiyat ~ Yaş + Kilometre + Q("Motor Hacmi") + Q("Motor Gücü") + Q("Ortalama Yakıt Tüketimi") + Q("Depo Hacmi")', data=df)
+fit1 = lsm.fit()
+
+# Get the R-squared score
+r2_score = fit1.rsquared
+
+print("R-squared Score:", r2_score)
+
+# Log R-squared score using MLflow
+with mlflow.start_run():
+    mlflow.log_param("Model", "Linear Regression")
+    mlflow.log_metric("R2 Score", r2_score)
